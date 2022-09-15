75aacc496a7a3954cf1fc8fd99aa7e4bdb6415f7
waybro <a.ultanov@bk.ru>
2022-09-15 22:11:03 +0700

    cd 25

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -1,12 +1,7 @@
 from pathlib import Path
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-s9@5er^&uq)u04cj&z3-ud2xd41s4!lf94c*t=v%-826@+qufb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
