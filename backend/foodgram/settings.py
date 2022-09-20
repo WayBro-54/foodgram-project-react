253cae916ce5c833cb3abc47bfb23252e9e411f9
waybro <a.ultanov@bk.ru>
2022-09-20 18:22:27 +0700

    review 3

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -93,10 +93,10 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": (
-        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+        "rest_framework.permissions.AllowAny",
     ),
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.AllowAny",
     ),
     "DEFAULT_PAGINATION_CLASS": 'rest_framework.pagination.PageNumberPagination',
     "PAGE_SIZE": 6,
