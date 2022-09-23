6587a65d33ff0300fc54de7a728e9a37125b3c73
waybro <a.ultanov@bk.ru>
2022-09-23 22:58:21 +0700

    review 3

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.getenv(
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['51.250.17.79']
+ALLOWED_HOSTS = ['51.250.17.79', '*']
 
 # os.getenv('ALLOWED_HOSTS', '*')
 
@@ -100,14 +100,21 @@ DJOSER = {
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+#         'NAME': os.getenv('DB_NAME', 'postgres'),
+#         'USER': os.getenv('POSTGRES_USER', 'waybro'),
+#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'Gatinap54'),
+#         'HOST': os.getenv('DB_HOST', "127.0.0.1"),
+#         'PORT': os.getenv('DB_PORT', 5432),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', 'postgres'),
-        'USER': os.getenv('POSTGRES_USER', 'waybro'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'Gatinap54'),
-        'HOST': os.getenv('DB_HOST', "127.0.0.1"),
-        'PORT': os.getenv('DB_PORT', 5432),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
