a377e29af1d18d5b79534269128d7a141c44ddcd
waybro <a.ultanov@bk.ru>
2022-09-26 20:26:06 +0700

    review 3

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -42,6 +42,12 @@ class Ingredient(models.Model):
         ordering = ('name', )
         verbose_name = 'Ингредиент'
         verbose_name_plural = 'Ингредиенты'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('name', 'measurement_unit'),
+                name='name_measurement_unit_unique'
+            )
+        ]
 
     def __str__(self):
         return self.name
